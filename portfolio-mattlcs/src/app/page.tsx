76b765e0515c83889f7c5fcae0ec08c5caa61f03
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-06-07 19:32:49 -0300

    setting up

--- portfolio-mattlcs/src/app/page.tsx
+++ portfolio-mattlcs/src/app/page.tsx
@@ -2,16 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <p>
-        Test1
-      </p>
-      <p>
-        Test2
-      </p>
-      <p>
-        Test3
-      </p>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="">
+        <p>Welcome to my site</p>
+      </div>
+      
+      
+
+      <div className="">
+        <p>I'm Matt, this is my Portofoloi</p>
+      </div>
+
+      <div className="">
+        <p>footer</p>
+      </div>
     </div>
   );
 }
