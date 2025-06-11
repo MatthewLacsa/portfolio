052e0c74c52839a8d6cd5618ac3a6e15c6b190bc
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-06-11 12:25:32 -0300

    portfolio website name and description

--- portfolio-mattlcs/src/app/page.tsx
+++ portfolio-mattlcs/src/app/page.tsx
@@ -1,21 +1,35 @@
-import Image from "next/image";
+"use client"
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <div className="">
-        <a href="portfolio-mattlcs\src\pages\AboutMe.tsx">About Me</a>
-        <a href="portfolio-mattlcs\src\pages\Projects.tsx">Projects</a>
+      <div className="flex gap-4">
+        <a href="/aboutme">About Me</a>
+        <a href="/projects">Projects</a>
       </div>
       
-      
-
-      <div className="">
-        <p>I'm Matt, this is my Portofoloi</p>
+      <div className="text-7xl">
+        <p>Matthew Lacsa</p>
       </div>
 
       <div className="">
-        <p>footer</p>
+         <TypeAnimation
+            sequence={[
+            'CS @ Dalhousie University',
+            1000, 
+            'Aspiring Software Developer',
+            1000,
+            'Data Entry Clerk @ Halina',
+            1000,
+            'Code Reviewer @ Outlier',
+            1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: '2em', display: 'inline-block' }}
+            repeat={Infinity}
+          />
       </div>
     </div>
   );
