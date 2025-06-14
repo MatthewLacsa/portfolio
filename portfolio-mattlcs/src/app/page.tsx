4daf61979342e4aef9ec3420037dc449bde0fe23
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-06-13 21:30:08 -0300

    added animation and gradient on name

--- portfolio-mattlcs/src/app/page.tsx
+++ portfolio-mattlcs/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </div>
       
        <motion.h1
-      className="text-[4rem] font-bold text-blue-300 text-center"
+      className="text-[4rem] font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-indigo-600 text-center"
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
