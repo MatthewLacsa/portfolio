500f620ce3c0d9e6c6040e2f582f309f9d1c43ed
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-06-19 20:40:12 -0300

    changed colors and will now proceed with about me page

--- portfolio-mattlcs/src/app/page.tsx
+++ portfolio-mattlcs/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
 
       <motion.h1
-      className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#A8D6E0] to-[#8C8CFF] text-center"
+      className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#ecfcff] to-[#6d6d9c] text-center"
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 0.40, scale: 1 }}
       transition={{
@@ -22,17 +22,17 @@ export default function Home() {
      <div className="flex gap-6 text-2xl">
 
                 <a 
-                className="bg-clip-text  bg-gradient-to-r hover:from-[#ACD5E3] hover:to-[#9AE3B9] underline decoration-white underline-offset-4 hover:scale-110"
+                className="bg-clip-text  bg-gradient-to-r hover:from-[#ecfcff] hover:to-[#d9d9f3] underline decoration-white underline-offset-4 hover:scale-110"
                 href="/aboutme">About Me</a>
                 <a 
-                className="bg-clip-text  bg-gradient-to-r hover:from-[#ACD5E3] hover:to-[#9AE3B9] underline decoration-white underline-offset-4 hover:scale-110"
+                className="bg-clip-text  bg-gradient-to-r hover:from-[#ecfcff] hover:to-[#d9d9f3] underline decoration-white underline-offset-4 hover:scale-110"
                 href="/projects">Projects</a>
 
       </div>
     </motion.h1>
       
     <motion.h1
-      className="text-5xl sm:text-7xl md:text-8xl lg:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-indigo-600 text-center"
+      className="text-5xl sm:text-7xl md:text-8xl lg:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#ecfcff] to-[#6d6d9c] text-center"
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 0.40, scale: 1 }}
       transition={{
@@ -57,7 +57,7 @@ export default function Home() {
       <div className="text-xl sm:text-xl md:text-xl lg:text-2xl flex gap-3 mt-3">
         <IconButton imgSrc="/linkedin.png" altImg="linkedin" linkToContact="https://www.linkedin.com/in/matthewlacsa/"/>
         <IconButton imgSrc="/github.png" altImg="github" linkToContact="https://github.com/MatthewLacsa"/>
-        <IconButton imgSrc="/gmail.png" altImg="gmail" linkToContact="matthewlacsa@gmail.com"/>
+        <IconButton imgSrc="/gmail.png" altImg="gmail" linkToContact="mailto:matthewlacsa@gmail.com"/>
       </div>
     </motion.h1>
 
