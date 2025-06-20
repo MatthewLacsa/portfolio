08253338ddf550757a843d58598d04356432fab7
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-06-20 20:26:17 -0300

    stack updated to 2, will put the experience in the projects instead

--- portfolio-mattlcs/src/app/aboutme/page.tsx
+++ portfolio-mattlcs/src/app/aboutme/page.tsx
@@ -16,7 +16,7 @@ const AboutMe: React.FC = () => {
                         ease: [0, 0.71, 0.2, 1.01],
                     }}
                     className="flex flex-row gap-4 mt-10 w-150">
-                    <p className="text-3xl font-bold">Hey, My name is Matthew Lacsa! <Image src="/me.jpeg" alt="Profile" width="120" height="120" 
+                    <p className="text-3xl font-bold">Hey, My name is <span>Matthew Lacsa</span>! <Image src="/me.jpeg" alt="Profile" width="120" height="120" 
                     className="rounded-full object-cover float-right"/>
                        I am a Computer Science student @ <span>Dalhousie University</span>. 
                        I am currently on my 3rd year and I am an aspiring <span>Software Developer</span>. 
@@ -40,7 +40,7 @@ const AboutMe: React.FC = () => {
                     className={`relative h-28 mt-10 z-5`}
                     onClick={() => setExpandedStack(expandedStack === 0 ? null : 0)}
                 >
-                    <div className={`transition-all duration-500 ${expandedStack === 0 ? 'relative space-y-2 size-50' : 'stack size-50'} hover:scale-110`}>
+                    <div className={`transition-all duration-500 ${expandedStack === 0 ? 'relative space-y-2 size-50' : 'stack size-50'} hover:scale-110 w-100`}>
                         <div className="border-base-content card bg-base-100 border text-center h-50">
                             <div className="card-body">A</div>
                         </div>
@@ -64,7 +64,7 @@ const AboutMe: React.FC = () => {
                     className={`relative h-28 mt-10 z-5` }
                     onClick={() => setExpandedStack(expandedStack === 1 ? null : 1)}
                 >
-                    <div className={`transition-all duration-500 ${expandedStack === 1 ? 'relative space-y-2 size-50' : 'stack size-50'} hover:scale-110`}>
+                    <div className={`transition-all duration-500 ${expandedStack === 1 ? 'relative space-y-2 size-50' : 'stack size-50'} hover:scale-110 w-100`}>
                         <div className="border-base-content card bg-base-100 border text-center h-50">
                             <div className="card-body">A</div>
                         </div>
@@ -77,29 +77,7 @@ const AboutMe: React.FC = () => {
                     </div>
                 </motion.div>
 
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{
-                        duration: 2,
-                        delay: 0.5,
-                        ease: [0, 0.71, 0.2, 1.01],
-                    }}
-                    className={`relative h-28 mt-10 z-5`}
-                    onClick={() => setExpandedStack(expandedStack === 2 ? null : 2)}
-                >
-                    <div className={`transition-all duration-500 ${expandedStack === 2 ? 'relative space-y-2 size-50' : 'stack size-50'} hover:scale-110`}>
-                        <div className="border-base-content card bg-base-100 border text-center h-50">
-                            <div className="card-body">A</div>
-                        </div>
-                        <div className="border-base-content card bg-base-100 border text-center h-50">
-                            <div className="card-body">B</div>
-                        </div>
-                        <div className="border-base-content card bg-base-100 border text-center h-50 ">
-                            <div className="card-body">C</div>
-                        </div>
-                    </div>
-                </motion.div>
+
              <div className="absolute bottom-0 left-0 w-full overflow-hidden -z-0 text-[0]">
                 {/* This is the wave at the very back*/}
                 <svg
